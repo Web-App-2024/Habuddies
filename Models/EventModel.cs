717cdadadc3c62d94dfae70610df7982ae234d0b
pookantong <pookantong.p@gmail.com>
2024-02-29 13:15:34 +0700

    fix(Event): fix schema

--- Models/EventModel.cs
+++ Models/EventModel.cs
@@ -7,20 +7,19 @@ public class Event
 {
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
-    public string? id { get; set; }
-
-    public required string title { get; set; }
-
-    public string? description { get; set; } = null!;
-
-    public required string category { get; set; }
-
-    public required int enrollSize { get; set; }
-
-    [BsonDateTimeOptions(Kind = DateTimeKind.Utc)]
-    public required DateTime endDate { get; set; }
-
-    public string? coverImage { get; set; } = null!;
-
-    public bool? isOpen { get; set; } = true;
+    public string Id { get; set; }
+    public string Title { get; set; }
+    public string OwnerId { get; set; }
+    public string Category { get; set; }
+    public int EnrollSize { get; set; }
+    public string Description { get; set; }
+    public DateTime EndDate { get; set; }
+    public string CoverImage { get; set; }
+    public bool IsOpen { get; set; }
+    public List<string> Subscribers { get; set; }
+    public List<string> Queue { get; set; }
+    public int AgeRequirement { get; set; }
+    public string GenderRequirement { get; set; }
+    public DateTime StartEvent { get; set; }
+    public DateTime EndEvent { get; set; }
 }
