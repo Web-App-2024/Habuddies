f7f9148fb8f3aa14f1ca4d6fc9cc50c52a1fdc92
pookantong <pookantong.p@gmail.com>
2024-03-07 19:17:10 +0700

    Revert "fix(User): update JoinedEvent"
    This reverts commit ad164717fc6bb085534d72b0cddf959611e6479a.

--- Services/EventService.cs
+++ Services/EventService.cs
@@ -191,10 +191,6 @@ namespace HaBuddies.Services
                 }
 
                 await _eventsCollection.DeleteOneAsync(evt => evt.Id == id);
-
-                var updateFilter = Builders<User>.Filter.ElemMatch(u => u.JoinedEvent, id);
-                var update = Builders<User>.Update.Pull(u => u.JoinedEvent, id);
-                await _usersCollection.UpdateManyAsync(updateFilter, update);
             }
             catch (Exception) {
                 throw;
@@ -206,8 +202,6 @@ namespace HaBuddies.Services
             try {
                 var user = await _usersCollection.Find(u => u.Id == userId).FirstOrDefaultAsync();
                 var evt = await GetOneAsync(id);
-                var filterUser = Builders<User>.Filter.Eq(u => u.Id, userId);
-                
 
                 if (evt == null) {
                     throw new Exception("Event Not Found");
@@ -230,8 +224,6 @@ namespace HaBuddies.Services
                     if (evt.SubscribersId.Count < evt.EnrollSize){
                         await _eventsCollection.UpdateOneAsync(filter, 
                             Builders<Event>.Update.Push(evt => evt.SubscribersId, userId));
-                        await _usersCollection.UpdateOneAsync(filterUser, 
-                            Builders<User>.Update.Push(u => u.JoinedEvent, evt.Id));
                     }
                     else if (evt.SubscribersId.Count >= evt.EnrollSize){
                         await _eventsCollection.UpdateOneAsync(filter, 
@@ -242,8 +234,6 @@ namespace HaBuddies.Services
                     if(evt.SubscribersId.Contains(userId)){
                         await _eventsCollection.UpdateOneAsync(filter,
                             Builders<Event>.Update.Pull(evt => evt.SubscribersId, userId));
-                        await _usersCollection.UpdateOneAsync(filterUser, 
-                            Builders<User>.Update.Pull(u => u.JoinedEvent, evt.Id));
 
                         if (evt.QueueId.Count > 0)
                         {
@@ -251,8 +241,6 @@ namespace HaBuddies.Services
                             await _eventsCollection.UpdateOneAsync(filter,
                                 Builders<Event>.Update.Push(evt => evt.SubscribersId, userFromQueueId)
                                     .Pull(evt => evt.QueueId, userFromQueueId));
-                            await _usersCollection.UpdateOneAsync(u => u.Id == userFromQueueId, 
-                                Builders<User>.Update.Push(u => u.JoinedEvent, evt.Id));
                         }
                     }
                     else {
@@ -276,13 +264,9 @@ namespace HaBuddies.Services
                 if (DateTime.UtcNow > evt.EndDate)
                 {
                     evt.IsOpen = false;
-                    var updateEvent = Builders<Event>.Update.Set(e => e.IsOpen, false);
-                    await _eventsCollection.UpdateOneAsync(e => e.Id == evt.Id, updateEvent);
+                    var update = Builders<Event>.Update.Set(e => e.IsOpen, false);
+                    await _eventsCollection.UpdateOneAsync(e => e.Id == evt.Id, update);
                     Console.WriteLine($"Close Event Id{evt.Id}");
-
-                    var filterUser = Builders<User>.Filter.In(u => u.Id, evt.SubscribersId);
-                    var update = Builders<User>.Update.Push(u => u.JoinedEvent, evt.Id);
-                    await _usersCollection.UpdateManyAsync(filterUser, update);
                 }
             }
         }
