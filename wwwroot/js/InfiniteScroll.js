4cef54f61664e3903b6e4258844a071d9ab6e8c0
ReallyWarm <warmkitjarak@gmail.com>
2024-03-08 18:42:05 +0700

    refactor: move js to external file for infinite scroll and join button

---
+++ wwwroot/js/InfiniteScroll.js
@@ -0,0 +1,56 @@
+function EventInfiniteScroll(divId, route) {
+    var xmlhttp = new XMLHttpRequest();
+    const divBody = document.getElementById(divId);
+    this.route = route;
+    this.loading = false;
+    this.hasItem = true;
+    this.category = "";
+    this.page = 1;
+    this.perPage = 10;
+    this.data = "";
+    var self = this;
+
+    this.setCategory = function(category) {
+        self.category = category;
+    }
+
+    this.loadEvent = function(refresh=false) {
+        xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+                if (xmlhttp.status == 200) {
+                    divBody.insertAdjacentHTML('beforeEnd', xmlhttp.responseText);
+                    self.loading = false;
+                    self.page += 1;
+                }
+                else if (xmlhttp.status == 204) {
+                    self.hasItem = false;
+                }
+            }
+        };
+
+        if (refresh) {
+            divBody.innerHTML = "";
+            self.page = 1;
+            self.hasItem = true;
+        }
+        
+        if (self.hasItem) {
+            self.data = 'category=' + encodeURIComponent(self.category) +
+                        '&page=' + encodeURIComponent(self.page) +
+                        '&perPage=' + encodeURIComponent(self.perPage);
+
+            self.loading = true;
+            xmlhttp.open("POST", self.route, true);
+            xmlhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+            xmlhttp.send(self.data);
+        }
+    };
+
+    window.onscroll = function (ev) {
+        if ((window.innerHeight + window.scrollY) >= (document.documentElement.scrollHeight)) {
+            if (!self.loading) {
+                self.loadEvent(false);
+            }
+        }
+    };
+};
