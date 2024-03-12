22c5e9144311b50f896f559a3a43adafb78405d0
PcrPz <pacharaeiamsongkram@gmail.com>
2024-03-13 00:11:26 +0700

    NotificationOwner

--- wwwroot/js/notification.js
+++ wwwroot/js/notification.js
@@ -12,3 +12,19 @@ function toggleNotification() {
     down = true;
   }
 }
+
+var btn = document.getElementById("btn");
+var owner = document.getElementById("notification-owner");
+var user = document.getElementById("notification-user");
+
+function leftClick() {
+  btn.style.left = "0";
+  user.style.display = "Block";
+  owner.style.display = "None";
+}
+
+function rightClick() {
+  btn.style.left = "125px";
+  user.style.display = "None";
+  owner.style.display = "Block";
+}
